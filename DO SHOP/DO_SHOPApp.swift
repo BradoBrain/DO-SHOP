9650c1fa1126918224216a60628a48d2976f3817
BB <izum_music@icloud.com>
2022-04-08 18:20:48 +0300

    Initial Commit

---
+++ DO SHOP/DO_SHOPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DO_SHOPApp.swift
+//  DO SHOP
+//
+//  Created by Artem Vinogradov on 08.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct DO_SHOPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
